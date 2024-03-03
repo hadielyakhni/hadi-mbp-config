5e6e8d9b013b21f505e8dc8415fdbda2488b3c52
Anton Romanova <contact@antonromanova.com>
2024-03-03 18:58:15 +0100

    Update zsh configuration

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,11 +1,14 @@
 {...}: {
   programs.zsh = {
-    enable = true;
     autocd = true;
-    history.share = true;
+    defaultKeymap = "emacs";
+    # TODO: add dirHashes: https://blog.myelin.ch/2018/12/14/named-directory-hash-table.html
+    #  https://mynixos.com/home-manager/option/programs.zsh.dirHashes
+    enable = true;
     enableAutosuggestions = true;
     enableCompletion = true;
-    enableSyntaxHighlighting = true;
+    history.share = true;
+    syntaxHighlighting.enable = true;
     historySubstringSearch.enable = true;
     oh-my-zsh = {
       enable = true;
@@ -16,14 +19,11 @@
       theme = "fishy";
     };
     initExtra = ''
-      ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=5'
+      ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg(=5'
     '';
   };
-
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
   };
-
-
 }
