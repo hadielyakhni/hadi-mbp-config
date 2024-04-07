afdfdee50366861dbb02ad622f62e9b041c63455
Anton Romanova <contact@antonromanova.com>
2024-04-07 13:32:04 +0200

    Add java17 for minecraft

--- home/minecraft.nix
+++ home/minecraft.nix
@@ -2,6 +2,8 @@
 {
   home.packages = with pkgs; [
      prismlauncher-unwrapped # oss minecraft launcher
+     zulu17
   ];
   home.file."Applications/PrismLauncher.app".source = "${pkgs.prismlauncher-unwrapped}/PrismLauncher.app";
+  home.file."Library/Application Support/PrismLauncher/java".source = "${pkgs.zulu17}";
 }
