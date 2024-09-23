5d62e64540dc61ad0fb93aa8239afc3df9f6f59b
Anton Romanova <contact@antonromanova.com>
2024-09-23 14:33:10 +0200

    Add owl vision extension to vscode

--- home/vscode.nix
+++ home/vscode.nix
@@ -9,6 +9,13 @@
         ms-vscode-remote.remote-ssh
         astro-build.astro-vscode
         github.copilot
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "owl-vision";
+          publisher = "Odoo";
+          version = "0.1.0";
+          sha256 = "sha256-VbCKx8WvCx94m+2nHnF0hpgL0o0xNCyH6FZY88ccVL4=";
+        }
       ];
     })
   ];
