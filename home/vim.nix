aae642d04f5d7a499a04da04652f8379d6e6c22c
Hadi El Yakhni <hael@odoo.com>
2025-02-28 08:49:31 +0100

    Remove NerdTree keymaps for now
    Since I am not yet using it. To be researched later...

--- home/vim.nix
+++ home/vim.nix
@@ -91,9 +91,6 @@
 	-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 	vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
-	-- Nerdtree
-	vim.keymap.set("n", "<C-n>", "<cmd>NERDTreeToggle<CR>")
     '';
   };
 }
