9af3af901f545e4134f2ed8323c9486e1eeda126
Hadi El Yakhni <hadiyakhni33@gmail.com>
2025-12-09 10:54:28 +0100

    remove mac-app-utils
    now that i have moved all the apps to be insatlled with homebrew casks

--- flake.nix
+++ flake.nix
@@ -15,11 +15,9 @@
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
-
-    mac-app-util.url = "github:hraban/mac-app-util";
   };
 
-  outputs = inputs @ { self, nixpkgs-darwin, nixpkgs-unstable, darwin, home-manager, mac-app-util, ... }: let
+  outputs = inputs @ { self, nixpkgs-darwin, nixpkgs-unstable, darwin, home-manager, ... }: let
     username = "hadielyakhni";
     system = "aarch64-darwin";
     hostname = "${username}-mbp";
@@ -48,15 +46,11 @@
         ./modules/homebrew.nix
         ./modules/system.nix
         ./modules/zsh.nix
-        mac-app-util.darwinModules.default
         home-manager.darwinModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.extraSpecialArgs = specialArgs;
-          home-manager.sharedModules = [
-            mac-app-util.homeManagerModules.default
-          ];
           home-manager.users.${username} = import ./home {
             pkgs = specialArgs.pkgs;
             unstablePkgs = specialArgs.unstablePkgs;
